59db6dad8bbeeae18c0ac3783ae81e368ed9d40b
wtao <78861190@qq.com>
2017-08-20 15:38:20 +0800

    测试

---
+++ Summernote.php
@@ -0,0 +1,20 @@
+<?php
+namespace iqianfang\summernote;
+use yii\bootstrap\Widget;
+/**
+ * Created by PhpStorm.
+ * User: 山东千方信息科技 qianfang.me
+ * Date: 2017/8/20
+ * Time: 15:27
+ */
+class Summernote extends Widget
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run(){
+        return '11111';
+    }
+}
